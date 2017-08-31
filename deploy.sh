56985da43fe589cc861f821b15c97582d54cde21
Kai Hendry <hendry@iki.fi>
2017-08-31 15:26:52 +0800

    Deploy via script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-linux-amd64-latest
+sudo chmod +x /usr/local/bin/ecs-cli
+/usr/local/bin/ecs-cli configure -c $TRAVIS_BRANCH
+envsubst < docker-compose.yml.template > docker-compose.yml
+cat docker-compose.yml
+/usr/local/bin/ecs-cli compose -p $TRAVIS_BRANCH service up
+
